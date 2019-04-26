f96ec0c59127b6ef00bebcaea5dc463598ed8d08
Youngjune Kwon <shining@uvas.kr>
2019-04-26 12:55:32 +0900

    sampling

--- src/argos/rpi.py
+++ src/argos/rpi.py
@@ -1,18 +1,40 @@
-import spidev, time, datetime
-
-def sample():
+def sampling(channel=0, seconds=1, freq_hz=1000, max_speed_hz=1350000, retry=30, adjust_strength=1.5, tolerance=0):
+    from datetime import datetime
+    import spidev, time
     spi = spidev.SpiDev()
     spi.open(0,0)
-    spi.max_speed_hz = 1350000 
+    spi.max_speed_hz = max_speed_hz 
 
-    begin = datetime.datetime.now()
-    samples = []
-    channel = 0
+    def estimate_time():
+        arr = []
+        begin = datetime.now()
+        arr.append(spi.xfer2([1, (0x08+channel)<<4, 0]))
+        return (datetime.now() - begin).total_seconds()
 
-    print("begin, ", str(datetime.datetime.now()))
-    while (datetime.datetime.now() - begin).total_seconds() <= 1:
-        samples.append(spi.xfer2([1, (0x08+channel)<<4, 0]))
-        print(samples[-1])
+    def do_sample(s):
+        begin = datetime.now()
+        samples = []
+        while (datetime.now() - begin).total_seconds() <= seconds:
+            samples.append(spi.xfer2([1, (0x08+channel)<<4, 0]))
+            if s > 0:
+                time.sleep(s)
+        return samples
 
-    print(len(samples))
-    print("end, ", str(datetime.datetime.now()))
+    s = (1 / freq_hz) - estimate_time()
+    expected = freq_hz * seconds
+
+    result = []
+    if s >= 0:
+        for i in range(retry):
+            result = do_sample(s)
+            print(len(result))
+            dr = abs(len(result) - expected) / expected
+            if dr <= tolerance:
+                break
+            s *= 1 + dr * adjust_strength * (1 if len(result) > expected else -1)
+    else:
+        samples = do_sample(0)
+        step = len(samples) / expected
+        for i in range(expected):
+            result.append(samples[int(i * step)])
+    return result
